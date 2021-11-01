31b665d8e1509c05c853fc6573b4924e6b27413c
xhailey <panhailey2017@gmail.com>
2021-10-31 20:38:32 -0700

    update

--- big-data-processing-toolbox-app/src/App.js
+++ big-data-processing-toolbox-app/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
 
 function App() {
+  var hadoopIp = "127.0.0.1";
+  var sparkIp = "127.0.0.1";
+  var jupyterIp = "127.0.0.1";
+  var sonarqubeIp = "127.0.0.1";
+
+  var hadoopUrl = "http://" + hadoopIp;
+  var sparkUrl = "http://" + sparkIp;
+  var jupyterUrl = "http://" + jupyterIp + "?token=easytoken";
+  var sonarqubeUrl = "http://" + sonarqubeIp;
   return (
     <div className="App">
       <header className="App-header">
@@ -11,19 +20,19 @@ function App() {
           Please select application that you want to run.
         </h4>
         <div>
-          <a className="App-link" target="_blank" href="/hadoop">
+          <a className="App-link" target="_blank" href={hadoopUrl}>
             Apache Hadoop
           </a>
           <br/><br/>
-          <a className="App-link" target="_blank" href="/spark">
+          <a className="App-link" target="_blank" href={sparkUrl}>
             Apache Spark
           </a>
           <br/><br/> 
-          <a className="App-link" target="_blank" href="/jupyter?token=easytoken" >
+          <a className="App-link" target="_blank" href={jupyterUrl} >
             Jupyter Notebook
           </a>
           <br/><br/> 
-          <a className="App-link" target="_blank" href="/sonar">
+          <a className="App-link" target="_blank" href={sonarqubeUrl}>
             SonarQube and SonarScanner
           </a>
         </div>
